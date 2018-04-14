99fd3d9553e4d4e3c7fb239ee21f9396d399e2b9
Matt Parsons <parsonsmatt@gmail.com>
2018-04-13 23:08:12 -0600

    oh

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,12 @@
-{-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE RecursiveDo                #-}
 
 module Main where
 
-import Control.Monad.ST
+a :: String
+a = "hello"
 
-theThing :: ST s ()
-theThing = pure ()
-
-weirdlyLocal :: ST s ()
-weirdlyLocal = theThing
-
-runSTIO :: (forall s. ST s a) -> IO a
-runSTIO x = pure (runST x)
-
-thisWorks :: IO ()
-thisWorks = mdo
-    let weirdlyLocal = theThing
-    runSTIO weirdlyLocal
-    runSTIO weirdlyLocal
-
-thisBreaks :: IO ()
-thisBreaks = mdo
-    runSTIO weirdlyLocal
-    let weirdlyLocal = theThing
-    runSTIO weirdlyLocal
-
-thisIsFine :: IO ()
-thisIsFine = mdo
-    runSTIO weirdlyLocal
-    let asdf = theThing
-    runSTIO asdf
+test :: IO ()
+test = do
+    rec putStrLn a
+    let a = 3 :: Int
+    print a
