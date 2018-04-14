c3ad34bc96a0889b7dd665b256916a10fae2f7db
Matt Parsons <parsonsmatt@gmail.com>
2018-04-13 22:44:19 -0600

    this succeeds

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,43 @@
+{-# LANGUAGE AllowAmbiguousTypes        #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE KindSignatures             #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE RecursiveDo                #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+import           Control.Monad.Fix
+import           Data.String            (IsString (..))
+import           Data.Typeable
+import           GHC.Exts               (Constraint, coerce)
+import           GHC.TypeLits
+
+import Control.Monad.ST
+
+
+weirdlyLocal :: ST s ()
+weirdlyLocal = pure ()
+
+runSTIO :: (forall s. ST s a) -> IO a
+runSTIO x = pure (runST x)
+
+largerFunction :: IO ()
+largerFunction = do
+    runSTIO $ do
+        weirdlyLocal
+
+    runSTIO $ do
+        weirdlyLocal
+
+    pure ()
